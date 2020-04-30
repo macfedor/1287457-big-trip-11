bd5ddfe1e8cba0d4d550b8e62b142fd16da4ddfd
macfedor <af-work@ya.ru>
2020-04-30 22:07:45 +0300

    del a useless check

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -3,16 +3,15 @@ import {MONTH_SHORT_NAMES} from "../consts.js";
 
 const createTripInfoCost = (tripPoints) => {
   let total = 0;
-  if (tripPoints.length) {
-    tripPoints.forEach((tripPoint) => {
-      total += tripPoint.price;
-      if (tripPoint.type.offers) { // пока мы берем офферы из типов событий. как приедут настоящие данные перепишем эту часть (т.к. пока хз, как там все организовано, то почему бы пока не сделать так?)
-        tripPoint.type.offers.forEach((offer) => {
-          total += +offer.price;
-        });
-      }
-    });
-  }
+
+  tripPoints.forEach((tripPoint) => {
+    total += tripPoint.price;
+    if (tripPoint.type.offers) { // пока мы берем офферы из типов событий. как приедут настоящие данные перепишем эту часть (т.к. пока хз, как там все организовано, то почему бы пока не сделать так?)
+      tripPoint.type.offers.forEach((offer) => {
+        total += +offer.price;
+      });
+    }
+  });
   return (
     `<p class="trip-info__cost">
       Total: €&nbsp;<span class="trip-info__cost-value">${total}</span>
